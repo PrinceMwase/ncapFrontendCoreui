9d080f0bb507400b7f145703b7ab732f8c776b89
PrinceMwase <princefranklinemwase@gmail.com>
2022-04-19 04:58:52 +0200

    modularised base axios query

--- src/utils/useAxios.js
+++ src/utils/useAxios.js
@@ -1,39 +1,20 @@
 import axios from 'axios'
-import jwt_decode from 'jwt-decode'
-import dayjs from 'dayjs'
-import { useContext } from 'react'
-import AuthContext from '../context/AuthContext'
+/**Modularized base query for all api calls */
+const axiosBaseQuery =
+  ({ baseUrl } = { baseUrl: '' }) =>
+  async ({ url, method, data }) => {
+    try {
+      const result = await axios({ url: baseUrl + url, method, data })
+      return { data: result.data }
+    } catch (axiosError) {
+      let err = axiosError
+      return {
+        error: {
+          status: err.response?.status,
+          data: err.response?.data || err.message,
+        },
+      }
+    }
+  }
 
-const baseURL = 'http://127.0.0.1:8000'
-
-const useAxios = () => {
-  const { authTokens, setUser, setAuthTokens } = useContext(AuthContext)
-
-  const axiosInstance = axios.create({
-    baseURL,
-    headers: { Authorization: `Bearer ${authTokens?.access}` },
-  })
-
-  axiosInstance.interceptors.request.use(async (req) => {
-    const user = jwt_decode(authTokens.access)
-    const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1
-
-    if (!isExpired) return req
-
-    const response = await axios.post(`${baseURL}/token/refresh/`, {
-      refresh: authTokens.refresh,
-    })
-
-    localStorage.setItem('authTokens', JSON.stringify(response.data))
-
-    setAuthTokens(response.data)
-    setUser(jwt_decode(response.data.access))
-
-    req.headers.Authorization = `Bearer ${response.data.access}`
-    return req
-  })
-
-  return axiosInstance
-}
-
-export default useAxios
+export default axiosBaseQuery
